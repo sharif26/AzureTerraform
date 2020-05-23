830c3175327d6f30b6fde782dfa91d9c3c0f362c
Sharif Ahmed <shiarif@gmail.com>
2020-05-23 16:22:01 -0400

    Update SelfRegisterMachine-Template.ps1

--- slotScaleSetFromImageDef/SelfRegisterMachine-Template.ps1
+++ slotScaleSetFromImageDef/SelfRegisterMachine-Template.ps1
@@ -1,11 +1,11 @@
 #sample run
-#.\SelfRegisterMachine-Template.ps1 -systemdbservername "maheshgautam-localhost.8ff678fa9950.database.windows.net" -slotworkerservername "maheshgautam-localhost.8ff678fa9950.database.windows.net" -dbusername "adminuser" -dbpassword "P@ssw0rd12345678910!"" -clustername "TSE Service Cluster"
+#.\SelfRegisterMachine-Template.ps1 -systemdbservername "{sql-mi}.8ff678fa9950.database.windows.net" -slotworkerservername "{sql-mi}.8ff678fa9950.database.windows.net" -dbusername "adminuser" -dbpassword "P@ssw0rd12345678910!"" -clustername "SFMC Service Cluster"
 
 param ($systemdbservername,$slotworkerservername,$dbusername,$dbpassword, $clustername)
 $global:systemDbConnectionString = 'Server=tcp:'+$systemdbservername+',1433;Persist Security Info=False;User ID='+$dbusername+';Password='+$dbpassword+';Initial Catalog=SystemDB'
 $global:slotDbConnectionString = 'Server=tcp:'+$slotworkerservername+',1433;Persist Security Info=False;User ID='+$dbusernamer+';Password='+$dbpassword+';Initial Catalog=SlotService'
 
-New-EventLog -LogName "Application" -Source "Exacttarget Slot Init"
+New-EventLog -LogName "Application" -Source "Exact Init"
 
 function Invoke-SQL-No-Count-Timeout {
     param(
@@ -15,16 +15,15 @@ function Invoke-SQL-No-Count-Timeout {
       )
 
     $cnn = New-Object system.data.SqlClient.SQLConnection($connectionString)
-
-	$cnn.Open()
+    $cnn.Open()
 	
-	$cmd = New-Object system.data.sqlclient.sqlcommand($sqlCommand, $cnn)
+    $cmd = New-Object system.data.sqlclient.sqlcommand($sqlCommand, $cnn)
     $cmd.CommandTimeout = $timeout;
     $retval = $cmd.ExecuteNonQuery()
-	if ( $retval -lt -1 )
+    if ( $retval -lt -1 )
     {
-		Write-Host "Failed with error code $($retval)"
-	}
+	Write-Host "Failed with error code $($retval)"
+    }
     
     $cnn.Close()
 
@@ -47,17 +46,17 @@ function Register-Slot-Machine {
     return Invoke-SQL-No-Count-Timeout $connectionString $sqlQuery 30
 }
 
-Write-EventLog -LogName "Application" -Source "Exacttarget Slot Init" -EventID 3001 -EntryType Information -Message "Registering machine with SystemDB" -Category 1 -RawData 10,20
+Write-EventLog -LogName "Application" -Source "Exact Init" -EventID 3001 -EntryType Information -Message "Registering machine with System" -Category 1 -RawData 10,20
 
 Register-Slot-Machine $global:systemDbConnectionString $clustername
 
-Write-EventLog -LogName "Application" -Source "Exacttarget Slot Init" -EventID 3001 -EntryType Information -Message "Registering machine with SlotDatabase" -Category 1 -RawData 10,20
+Write-EventLog -LogName "Application" -Source "Exact Init" -EventID 3001 -EntryType Information -Message "Registering machine with SlotDb" -Category 1 -RawData 10,20
 
 Register-Slot-Machine $global:slotDbConnectionString $clustername
 
 
-Set-Service "Exacttarget Slot Service" -StartupType "Automatic"
+Set-Service "Exact Service" -StartupType "Automatic"
 
 Write-EventLog -LogName "Application" -Source "Exacttarget Slot Init" -EventID 3001 -EntryType Information -Message "Starting ET Slot service" -Category 1 -RawData 10,20
 #start the service
-Start-Service "Exacttarget Slot Service"
+Start-Service "Exact Service"
